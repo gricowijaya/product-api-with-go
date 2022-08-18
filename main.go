c0a2d02b210a319168f22debcfb81256c98bbd54
Gede Rico Wijaya <gricowijaya@gmail.com>
2022-08-18 11:38:03 +0800

    add endpoint based on id for Article

--- main.go
+++ main.go
@@ -18,23 +18,38 @@ func returnAllArticle(w http.ResponseWriter, r *http.Request) {
   json.NewEncoder(w).Encode(Articles) // encode the struct that we will be served as a json.
 }
 
+
+func returnOneArticle(w http.ResponseWriter, r *http.Request) {
+  vars := mux.Vars(r) // getting the variable from the argument
+  key := vars["id"]
+  fmt.Println("Endpoint Hit: returnOneArticle")
+  fmt.Fprintf(w, "Key: ", key)
+  for _, article := range Articles {
+    if article.Id == key {
+      json.NewEncoder(w).Encode(article) // encode the json data baseon the id request
+    }
+  }
+}
+
 // using the net/http
 // func handleRequests() {
 //   http.HandleFunc("/api", homePage) // the route of the page {{url:8081}/api}
 //   http.HandleFunc("/api/article", returnAllArticle) // the route of the page {{url:8081}/api/article}
 //   log.Fatal(http.ListenAndServe(":8081", nil))
 // }
-
+// ----------------------------------------------------------------------------------------------------
 // using the gorilla/mux v2 
 func handleRequests() {
   myRouter := mux.NewRouter().StrictSlash(true)
   myRouter.HandleFunc("/api", homePage)
   myRouter.HandleFunc("/api/article", returnAllArticle)
+  myRouter.HandleFunc("/api/article/{id}", returnOneArticle) // get the one variables
 
   log.Fatal(http.ListenAndServe(":8081", myRouter))
 }
 
 type Article struct {
+  Id string `json: "id"` // as a string
   Title string `json: "title"`
   Desc string `json: "desc"`
   Content string `json: "content"`
@@ -47,8 +62,8 @@ var Articles []Article
 func main() {
   // populate the array
   Articles = []Article {
-    Article{Title: "Hello", Desc: "This is the Description for The first element of the array", Content: "This is the First Article Content"},
-    Article{Title: "Hello", Desc: "This is the Description for The second element of the array", Content: "This is the Second Article Content"},
+    Article{Id: "1", Title: "Hello", Desc: "This is the Description for The first element of the array", Content: "This is the First Article Content"},
+    Article{Id: "2", Title: "Hello", Desc: "This is the Description for The second element of the array", Content: "This is the Second Article Content"},
   }
   handleRequests() // call the handle request that'll call 
                    // the Homepage Endpoint Hit of route /
